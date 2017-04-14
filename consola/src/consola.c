bd04e73a68e98e0dfcac0b275ce5816963fd16fa
nicopriano <npriano@outlook.com>
2017-04-14 19:46:57 -0300

    se carga conf y log en consola y se conecta a kernel

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -12,15 +12,35 @@
 #include <stdlib.h>
 #include <shared-library/connect.h>
 
-int main(void) {
+#include "consola.h"
+
+console_cfg * console_config;
+t_log * console_log;
+
+int main(int argc, char ** argv) {
+
 	char* timeStart = temporal_get_string_time(); //usando commons
 	printf("Tiempo de Inicio del Proceso: %s\n", timeStart);
 	saludo();
 	connect_send("mi primer mensaje enviado :)"); //usando nuestra shared library
+
+	load_config(argv[0]);
+	log_create("log_principal", "CONSOLA", true, LOG_LEVEL_INFO);
+	log_info(console_log, "IP del Kernel: %s", console_config->ipAddress);
+	log_info(console_log, "Puerto: %d", console_config->port);
+
+	conectar(console_config->ipAddress, string_itoa(console_config->port));
+
 	return EXIT_SUCCESS;
 }
 
 int saludo() {
 	puts("¡Hola CONSOLA!");
 	return EXIT_SUCCESS;
+}
+
+load_config(char * path) {
+	t_config* cfg = config_create(path);
+	console_config->ipAddress = config_get_string_value(cfg, "IP_KERNEL");
+	console_config->port = config_get_int_value(cfg, "PORT_KERNEL");
 }
---
+++ consola/src/consola.h
@@ -0,0 +1,21 @@
+/*
+ * consola.h
+ *
+ *  Created on: 14/4/2017
+ *      Author: utnso
+ */
+
+#ifndef CONSOLA_H_
+#define CONSOLA_H_
+
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/log.h>
+
+typedef struct {
+	char* ipAddress;
+	int* port;
+} console_cfg;
+
+
+#endif /* CONSOLA_H_ */
