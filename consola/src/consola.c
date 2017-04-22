bd627f89e159430f45c02f266dbd53fcac3cdf27
nicopriano <npriano@outlook.com>
2017-04-22 17:07:07 -0300

    UI de consola

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -21,10 +21,26 @@ int main(int argc, char* argv[]) {
 	saludo();
 	connect_send("mi primer mensaje enviado :)"); //usando nuestra shared library
 	connect_to_socket("127.0.0.1", "46000");
-	while(1){
+	//connect_to_socket(console_config->ipAddress, string_itoa(console_config->port));
+
+	char* command = malloc(sizeof(char)*256);
+
+	printf("/***************************************\\n");
+	printf("| init path		: Iniciar Programa			|\n");
+	printf("| kill pid		: Finalizar Programa	|\n");
+	printf("| disconnect	: Desconectar Consola	|\n");
+	printf("| clean			: Limpiar Mensajes		|\n");
+	printf("\***************************************/\n");
+
+	fgets(command, 256, stdin);
+
+	int ret = read_command(command);
+
+	/*while(1){
 		sleep(20);
-	}
+	}*/
 	return EXIT_SUCCESS;
+
 }
 
 int saludo() {
@@ -32,8 +48,41 @@ int saludo() {
 	return EXIT_SUCCESS;
 }
 
-load_config(char * path) {
+void load_config(char * path) {
 	t_config* cfg = config_create(path);
 	console_config->ipAddress = config_get_string_value(cfg, "IP_KERNEL");
 	console_config->port = config_get_int_value(cfg, "PORT_KERNEL");
+}
+
+int read_command(char* command) {
+
+	char** palabras = string_split(command, " ");
+
+	if(palabras[0] == NULL) return -2;
+
+	if(palabras[0] == "init") {
+		if((sizeof(palabras)/sizeof(palabras[0])) != 2) {
+			printf("Cantidad de parametros erronea\n");
+			return -1;
+		}
+		else {
+
+		}
+	}
+	else if(palabras[0] == "kill") {
+		if((sizeof(palabras)/sizeof(palabras[0])) != 2) {
+			printf("Cantidad de parametros erronea\n");
+				return -1;
+		}
+		else {
+
+		}
+	}
+	else if(palabras[0] == "disconnect") {
+
+	}
+	else if(palabras[0] == "clean") {
+
+	}
+	else return -2;
 }
