03e1996f5dc1fb98d7c62aa8e753de3ad9540e94
Dante Romero Lopez <rl.dante@gmail.com>
2017-05-17 22:32:58 -0300

    Memoria: inicio proceso, escritura, funciones dump

--- shared-library/shared-library/memory_prot.h
+++ shared-library/shared-library/memory_prot.h
@@ -13,16 +13,16 @@
 #define SHARED_LIBRARY_MEMORY_PROTOCOL_H_
 
 #define INIT_PROCESS_OC     			1
-#define	ASSIGN_PAGE_OC 						2
-#define	READ_OC			 							3
-#define	WRITE_OC									4
-#define	END_PROCESS_OC						5
+#define	ASSIGN_PAGE_OC 					2
+#define	READ_OC			 				3
+#define	WRITE_OC						4
+#define	END_PROCESS_OC					5
 
-#define	SUCCESS										1
-#define	ERROR											200
+#define	SUCCESS							1
+#define	ERROR							200
 #define	DISCONNECTED_CLIENT				201
 #define	DISCONNECTED_SERVER				202
-#define	ENOSPC										203
+#define	ENOSPC							203
 
 /**	╔═════════════════════════════════╗
 	║ MEMORY - RECEIVE OPERATION CODE ║
